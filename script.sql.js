3d4d08792127f8ed9b135cb81e7f1ed2dff9cb90
mvmoshchovskyi <mvmoshchovskyi@gmail.com>
2020-05-10 16:13:09 +0300

    last

--- script.sql.js
+++ script.sql.js
@@ -20,8 +20,9 @@
 // SELECT * FROM client  JOIN department d on client.Department_idDepartment = d.idDepartment
 // where departmentCity = 'Kyiv';
 // # 7. +Вивести імена клієнтів та їхні номера телефону, погрупувавши їх за іменами.
-// select FirstName from client
-// order by FirstName;
+// #  ?????????????? такого поля як телефон немає в таблиці .
+// select FirstName , LastName , city  from client
+// order by LastName;
 // # 8. +Вивести дані про клієнтів, які мають кредит більше ніж на 5000 тисяч гривень.
 // select *from client join application a on client.idClient = a.Client_idClient
 // where Currency = 'gryvnia' and sum > 5000;
@@ -33,9 +34,9 @@
 // select firstName,  lastName, sum from client join application a on client.idClient = a.Client_idClient
 // order by sum desc;
 // # 11. Визначити кількість заявок на крдеит для кожного клієнта.
-// select firstName,  lastName , idApplication from client join application a on client.idClient = a.Client_idClient
+// select firstName,  lastName , idApplication from client join application a on client.idClient = a.Client_idClient;
 // # 12. Визначити найбільший та найменший кредити.
-// select MAX(sum) AS 'Maximum Credit' , MIN(sum) AS 'Minimum Credit' from application
+// select MAX(sum) AS 'Maximum Credit' , MIN(sum) AS 'Minimum Credit' from application;
 // # 13. Порахувати кількість кредитів для клієнтів,які мають вищу освіту.
 // select sum(sum) as 'credits' from client join application a on client.idClient = a.Client_idClient
 // where Education = 'high';
@@ -73,7 +74,7 @@
 // delete from application
 // where CreditState = 'returned';
 // #######перевірка
-// select *from application
+// select *from application;
 // # 20. Видалити кредити клієнтів, в яких друга літера прізвища є голосною.
 // delete from application
 // WHERE Client_idClient IN
@@ -84,40 +85,42 @@
 //           or lastName like '_u%'
 //           or lastName like '_y%');
 // #######перевірка
-// select *from client join application a on client.idClient = a.Client_idClient
+// select *from client join application a on client.idClient = a.Client_idClient;
 // # Знайти львівські відділення, які видали кредитів на загальну суму більше ніж 5000
-// #
-// #
-// #
-// #
-// #
-// #
+// use bank;
+// select * from client
+//     join application a on client.idClient = a.Client_idClient
+//     join department d on client.Department_idDepartment = d.idDepartment
+// where DepartmentCity = 'lviv' and Sum > 5000;
 // # Знайти клієнтів, які повністю погасили кредити на суму більше ніж 5000
-// #
-// #
-// #
-// #
-// #
+// select * from client
+//     join application a on client.idClient = a.Client_idClient
+//     join department d on client.Department_idDepartment = d.idDepartment
+// where CreditState = 'returned' and sum > 5000;
 // # /* Знайти максимальний неповернений кредит.*/
-// #
-// #
-// #
-// #
+// select * from client join application a on client.idClient = a.Client_idClient
+// where CreditState = 'not returned'
+// order by sum desc
+// limit 1;
 // # /*Знайти клієнта, сума кредиту якого найменша*/
-// #
-// #
-// #
-// #
+// select *from client c join application a on c.idClient = a.Client_idClient
+// order by sum;
 // # /*Знайти кредити, сума яких більша за середнє значення усіх кредитів*/
-// #
-// #
-// #
+// SELECT * FROM application
+// WHERE sum > (SELECT AVG(sum) FROM application);
 // # /*Знайти клієнтів, які є з того самого міста, що і клієнт, який взяв найбільшу кількість кредитів*/
-// #
-// #
-// #
+// select count(sum) countCredit , city,LastName from client c
+//     join application a on c.idClient = a.Client_idClient
+// group by LastName
+// order by countCredit desc
+// limit 1;
+// select *from client c join application a on c.idClient = a.Client_idClient
+// where city like 'Krasne';
 // # #місто чувака який набрав найбільше кредитів
-// #
+// select sum(sum) mostSum , city, FirstName ,LastName from client c join application a on c.idClient = a.Client_idClient
+// group by LastName
+// order by mostSum desc
+// limit 1;
 // #
 // #
 // # set sql_safe_updates = 0;
